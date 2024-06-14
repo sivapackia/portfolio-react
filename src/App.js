78a0a6a35c5458e50b27d7710430eccf1f732cb1
R.SIVAPACKIA <sivapackia2001@gmail.com>
2024-06-14 15:53:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Router from "./portfolio/Router";
+import Store from "./portfolio/Store"
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Provider store={Store}>
+         <Router/>
+      </Provider>
     </div>
   );
 }
-
 export default App;
---
+++ src/portfolio/Reducer.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+import React from "react";
+export const Slice=createSlice(
+    {
+        name:"Portfolio",
+        initialState:{
+
+        },
+        reducers:{
+
+        }
+    }
+) 
+export default Slice.reducer
